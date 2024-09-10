4230a8b5e5512705b4afee117ce9c98e5160f2ca
Abran <158106702+AbranEG@users.noreply.github.com>
2024-09-10 18:23:40 +0000

    Sitio web dinámico

--- web.py
+++ web.py
@@ -9,22 +9,24 @@ class WebRequestHandler(BaseHTTPRequestHandler):
     def query_data(self):
         return dict(parse_qsl(self.url().query))
 
-def do_GET(self):
-    if self.path == "/":
-        
-        with open("home.html", "r") as file:
-            content = file.read()
+contenido = {
+    '/': """<html><h1>Inicio del Sitio Web</h1></html>""",
+    '/proyecto/1': """<html><h1>Aplicación para la Gestión de Tareas Diarias</h1></html>""",
+    '/proyecto/2': """<html><h1>Portafolio de Proyectos Creativos</h1></html>""",
+    '/proyecto/3': """<html><h1>Blog de Viajes y Experiencias</h1></html>"""
+}
 
+def do_GET(self):
+    if self.path in contenido:
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
-        self.wfile.write(content.encode("utf-8"))
+        self.wfile.write(contenido[self.path].encode("utf-8"))
     else:
         self.send_response(404)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
-        self.wfile.write("<h1>ERROR 404:Not Found</h1>".encode("utf-8"))
-
+        self.wfile.write("<h1>ERROR 404: Not Found</h1>".encode("utf-8"))
 
 def get_response(self, host, user_agent):
    
