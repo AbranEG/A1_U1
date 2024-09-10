c70d52ef3c9216d86c3f17d3c4bf6fec61db6879
Abran <158106702+AbranEG@users.noreply.github.com>
2024-09-10 18:12:42 +0000

    Contenido HTML generado dinámicamente

--- web.py
+++ web.py
@@ -9,20 +9,36 @@ class WebRequestHandler(BaseHTTPRequestHandler):
     def query_data(self):
         return dict(parse_qsl(self.url().query))
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.end_headers()
-        self.wfile.write(self.get_response().encode("utf-8"))
+def do_GET(self):
+   
+    host = self.headers.get('Host')
+    user_agent = self.headers.get('User-Agent')
 
-    def get_response(self):
-        return f"""
-    <h1> Hola Web </h1>
-    <p> URL Parse Result : {self.url()}         </p>
-    <p> Path Original: {self.path}         </p>
-    <p> Headers: {self.headers}      </p>
-    <p> Query: {self.query_data()}   </p>
-"""
+   
+    self.send_response(200)
+
+  
+    self.send_header("Content-Type", "text/html")
+    self.send_header("Server", "CustomPythonServer")
+    self.send_header("Date", self.date_time_string())
+    self.end_headers()
+
+   
+    self.wfile.write(self.get_response(host, user_agent).encode("utf-8"))
+
+def get_response(self, host, user_agent):
+   
+    query_data = self.query_data()
+    autor = query_data.get('autor', 'desconocido')
+
+
+    return f"""
+    <h1>Proyecto: {self.url().path} Autor: {autor}</h1>
+    <p> Host: {host} </p>
+    <p> User-Agent: {user_agent} </p>
+    """
+
+
 
 if __name__ == "__main__":
     print("Starting server on port 8000")
